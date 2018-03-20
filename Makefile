a3840893cb45701a668dcbaa36b86e0374646eda
Sivan Mehta <Sivanmehta2@gmail.com>
2018-03-19 18:31:02 -0700

    [wip] auth route

--- Makefile
+++ Makefile
@@ -3,6 +3,8 @@ server: install build serve
 install:
 	go get github.com/gorilla/mux
 	go get github.com/gorilla/sessions
+	go get golang.org/x/oauth2
+	go get github.com/google/go-github/github
 
 build:
 	go build
--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
   "github.com/gorilla/mux"
   "github.com/gorilla/sessions"
+  "golang.org/x/oauth2"
+  "github.com/google/go-github/github"
 
   "log"
+  "fmt"
   "net/http"
   "html/template"
   "os"
@@ -21,7 +24,11 @@ var (
   tmpls = map[string]*template.Template{}
 
   store *sessions.CookieStore
+
+	oauthCfg *oauth2.Config
 )
+
+
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
   log.Println("Serve home route")
@@ -42,6 +49,46 @@ func StartHandler(w http.ResponseWriter, r *http.Request) {
   http.Redirect(w, r, "/", 302)
 }
 
+func AuthHandler(w http.ResponseWriter, r *http.Request) {
+  session, err := store.Get(r, "sess")
+  if err != nil {
+		fmt.Fprintln(w, "could not read session")
+		return
+	}
+
+  if r.URL.Query().Get("state") != session.Values["state"] {
+    fmt.Fprintln(w, "no state match; possible csrf OR cookies not enabled")
+    return
+  }
+
+  tkn, err := oauthCfg.Exchange(oauth2.NoContext, r.URL.Query().Get("code"))
+  if err != nil {
+    fmt.Fprintln(w, "there was an issue getting your token")
+    return
+  }
+
+  if !tkn.Valid() {
+		fmt.Fprintln(w, "retreived invalid token")
+		return
+	}
+
+  client := github.NewClient(oauthCfg.Client(oauth2.NoContext, tkn))
+  ctx := r.Context()
+
+  user, _,  err := client.Users.Get(ctx, "")
+	if err != nil {
+		fmt.Println(w, "error getting name")
+		return
+	}
+
+  session.Values["name"] = user.Name
+  session.Values["accessToken"] = tkn.AccessToken
+  session.Save(r, w)
+
+  http.Redirect(w, r, "/", 302)
+
+}
+
 func main () {
   tmpls["home.html"] = template.Must(template.ParseFiles(templateDir + "home.html", defaultLayout))
 
@@ -50,6 +97,7 @@ func main () {
   r := mux.NewRouter()
   r.HandleFunc("/", HomeHandler)
   r.HandleFunc("/start", StartHandler)
+  r.HandleFunc("/auth", AuthHandler)
 
   http.Handle("/", r)
   port := os.Getenv("PORT")
